fa0b415938be7d106e23868b8182f04e76c2837b
Megan Fago <meganlaynefago@gmail.com>
2019-01-16 22:19:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese(array)
- cheese_types = ["cheddar", "gouda", "camembert"]
+# cheese_types = ["cheddar", "gouda", "camembert"]
   if array.include?("cheddar")
     puts "cheddar"
   else 
