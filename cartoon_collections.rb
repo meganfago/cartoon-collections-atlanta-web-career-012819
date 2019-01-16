99294981274d939176feade8f30f06c16c41239f
Megan Fago <meganlaynefago@gmail.com>
2019-01-16 21:45:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+ dwarves.each.with_index(1) do |value, index|
+   puts "#{index}  #{value}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  veggies.collect do  {|x| x + "!"}
+  x.capitalize
+end
 end
 
 def long_planeteer_calls# code an argument here
