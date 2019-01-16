5af078b97f26ce17a39dcd8d744ec548cc4eb556
Megan Fago <meganlaynefago@gmail.com>
2019-01-16 22:09:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese(cheddar_cheese)
- cheese_types = ["cheddar", "gouda", "camembert"].include?
-  cheddar_cheese.include?("cheddar")
+ cheese_types = ["cheddar", "gouda", "camembert"]
+  cheddar_cheese.include?
 end
